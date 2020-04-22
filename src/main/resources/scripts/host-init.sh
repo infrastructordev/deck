849c44e095b746ca3e9898403a298b6a9f3d9e49
mle <michael@lewkowski.de>
2020-04-22 21:53:38 +0200

    Fix host-init script

--- src/main/resources/scripts/host-init.sh
+++ src/main/resources/scripts/host-init.sh
@@ -2,4 +2,4 @@
 
 curl -s "https://raw.githubusercontent.com/nl2go/ansible-pull-init/master/ansible-install.sh" | sh
 
-cat "* * * * * root curl -s {{hostHeartbeatUrl}}?hostToken={{hostToken}} | sh" > /etc/cron.d/infrastructr-deck-heartbeat
+echo "* * * * * root curl -s {{hostHeartbeatUrl}}?hostToken={{hostToken}} | sh" > /etc/cron.d/infrastructr-deck-heartbeat
